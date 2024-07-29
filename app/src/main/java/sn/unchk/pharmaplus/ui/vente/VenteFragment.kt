848c61d91eafb480b89203dd3c4e718eacfd0174
ndiayemnsour <zocod3r@gmail.com>
2024-07-29 15:45:56 +0000

    implemmentation de l'interface vente et modification du background de l'interface home

--- app/src/main/java/sn/unchk/pharmaplus/ui/vente/VenteFragment.kt
+++ app/src/main/java/sn/unchk/pharmaplus/ui/vente/VenteFragment.kt
@@ -1,4 +1,4 @@
-package sn.unchk.pharmaplus.ui.dashboard
+package sn.unchk.pharmaplus.ui.vente
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import sn.unchk.pharmaplus.databinding.FragmentDashboardBinding
+import sn.unchk.pharmaplus.databinding.FragmentVenteBinding
 
-class DashboardFragment : Fragment() {
+class VenteFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentVenteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,15 +22,15 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val venteViewModel=
+            ViewModelProvider(this).get(VenteViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentVenteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+
+        venteViewModel.text.observe(viewLifecycleOwner) {
+
         }
         return root
     }
--- app/src/main/java/sn/unchk/pharmaplus/ui/vente/VenteViewModel.kt
+++ app/src/main/java/sn/unchk/pharmaplus/ui/vente/VenteViewModel.kt
@@ -1,13 +1,13 @@
-package sn.unchk.pharmaplus.ui.dashboard
+package sn.unchk.pharmaplus.ui.vente
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class VenteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is vente Fragment"
     }
     val text: LiveData<String> = _text
 }
